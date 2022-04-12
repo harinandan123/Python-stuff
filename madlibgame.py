cf4c7338a63ccb1bc6e97c2565956fd2efbb94b0
Soundwave <100547398+harinandan123@users.noreply.github.com>
2022-04-12 14:50:47 +0530

    Create madlibgame.py

---
+++ madlibgame.py
@@ -0,0 +1,7 @@
+colour = input("Enter colour: ")
+celebrity = input("Enter celibrity: ")
+noun = input("Enter noun: ")
+
+print("Roses are " + colour)
+print("I love " + celebrity)
+print(noun + " is blue")
