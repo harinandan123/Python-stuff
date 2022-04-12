51c9e8f2ad1c41974cdb0f7c79615e95e9f54d5b
Soundwave <100547398+harinandan123@users.noreply.github.com>
2022-04-12 15:26:19 +0530

    Create whileloops.py

---
+++ Learning Python/whileloops.py
@@ -0,0 +1,24 @@
+# Note- if its an infinite while loop, click Ctrl + C (on windows)or Cmd + C (on mac) on the terminal to stop it
+print("Note- if its an infinite while loop, click Ctrl + C (on windows)or Cmd + C (on mac) on the terminal to stop it")
+
+
+
+i = 1
+while i <= 10:
+    print(i)
+    i += 1
+     
+
+
+print("Done with loop")
+
+
+# Another example
+while True:
+    print("Infinite Loop")
+
+
+# Another example
+x = 1
+while x == 1:
+    print("This is an infinite loop ") 
